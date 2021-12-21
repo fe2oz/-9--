54d3b2dacd143902b023689b61b9c5d8522cfd19
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-12-21 17:40:32 +0900

    12.21/17:40

--- js/main.js
+++ js/main.js
@@ -11,5 +11,41 @@ $(function(){
         $(".court_show").stop().slideUp("fast");
         $("#backg2").stop().slideUp("fast");
     })
+
+    $("#next").click(function(){
+        $("#slide_box").stop().animate({marginLeft:-685.41}, function(){
+            $("#slide_box li:first").appendTo("#slide_box");
+            $("#slide_box").css({marginLeft:0})
+        })
+    })
+
+    $("#prev").click(function(){
+        $("#slide_box").stop().animate({marginLeft:0}, function(){
+            $("#slide_box li:last").prependTo("#slide_box");
+            $("#slide_box").css({marginLeft:-685.41});
+        })
+    })
+
+    $("#change_court_image_play").click(function(){
+        $(this).attr("src", "img/211876_play_icon.png");
+    })
 })
 
+function slide(){
+    $("#slide_box").stop().animate({marginLeft:-685.41}, function(){
+        $("#slide_box li:first").appendTo("#slide_box");
+        $("#slide_box").css({marginLeft:0});
+    })
+}
+
+setInterval(slide, 2000);
+
+
+function slideTwo(){
+    $("#slide2_box").stop().animate({marginLeft:-265.11}, function(){
+        $("#slide2_box li:first").appendTo("#slide2_box");
+        $("#slide2_box").css({marginLeft:0})
+    })
+}
+
+setInterval(slideTwo, 2000);
